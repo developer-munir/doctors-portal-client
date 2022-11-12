7e2d9341b836bf1e9d79d08162eb0ae8e939361e
developer-munir <devolopermunir@gmail.com>
2022-11-12 17:14:54 +0600

    added time table section

--- src/pages/Home/Header/Info.jsx
+++ src/pages/Home/Header/Info.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Info = () => {
-    return (
-      <div className="h-[190px] flex py-14 px-6 ">
-        <div>
-          <h1>Icon</h1>
-        </div>
-        <div className='ml-6'>
-          <h1>content</h1>
-        </div>
+const Info = ({ icon,title }) => {
+  return (
+    <div className="h-[190px] flex items-center py-14 px-6 ">
+      <div>
+        <h1>{icon}</h1>
       </div>
-    );
+      <div className="ml-6">{title}</div>
+    </div>
+  );
 };
 
 export default Info;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,20 +2,55 @@ import React from "react";
 import Header from "../Header/Header";
 import Info from "../Header/Info";
 import "../Header/header.css";
-
+import { BsFillAlarmFill } from "react-icons/bs";
+import { HiLocationMarker } from "react-icons/hi";
+import { FcCallback } from "react-icons/fc";
 const Home = () => {
+  const alerm = (
+    <React.Fragment>
+      <BsFillAlarmFill size={50}></BsFillAlarmFill>
+    </React.Fragment>
+    );
+  const location = (
+      <React.Fragment>
+          <HiLocationMarker size={50}></HiLocationMarker>
+    </React.Fragment>
+    );
+  const phone = (
+      <React.Fragment>
+          <FcCallback size={50}></FcCallback>
+    </React.Fragment>
+    );
+    const title1 = (
+      <React.Fragment>
+            <h1>Opening Hours</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi et earum, dicta saepe </p>
+      </React.Fragment>
+    );
+    const title2 = (
+      <React.Fragment>
+        <h1>Visit our location</h1>
+        <p>Brooklyn, NY 10036, United States</p>
+      </React.Fragment>
+    );
+    const title3 = (
+      <React.Fragment>
+        <h1>Contact us now</h1>
+        <p>Contact us nowContact us now</p>
+      </React.Fragment>
+    );
   return (
     <div className="mx-[21px]">
       <Header></Header>
-      <div className="grid md:grid-cols-3 gap-6 relative bottom-3">
+      <div className="grid md:grid-cols-3 gap-6 relative bottom-3 mt-[80px] md:mt-0">
         <div className="opening-hours">
-          <Info></Info>
+          <Info icon={alerm} title={title1}></Info>
         </div>
-        <div className="bg-accent rounded-[14px]">
-          <Info></Info>
+        <div className="bg-accent rounded-[14px] text-white">
+          <Info icon={location} title={title2}></Info>
         </div>
         <div className="contact">
-          <Info></Info>
+          <Info icon={phone} title={title3}></Info>
         </div>
       </div>
     </div>
